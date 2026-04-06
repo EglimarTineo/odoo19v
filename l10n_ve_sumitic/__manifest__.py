c81cfc2a169a888145b1fc12a56862f75205b419
EglimarTineo <eglimart22@gmail.com>
2026-04-06 01:45:34 -0400

    Update __manifest__.py

--- l10n_ve_sumitic/__manifest__.py
+++ l10n_ve_sumitic/__manifest__.py
@@ -14,7 +14,6 @@
         "sale",
     ],
     "data": [
-        "data/l10n_ve_sumitic_chart_template.xml",
         "data/res_currency_data.xml",
         "data/template/account.tax.group.csv",
         "data/template/account.account.csv",
